2590da89da4a394386df3c767428eb2f93fb31e7
kuboeb <kuboebtilda@gmail.com>
2025-06-14 12:06:54 +0000

    Update database imports with new functions

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,23 +1,39 @@
 """
 Модуль базы данных
 """
-from .models import init_db, Application
+from .models import init_db, Application, UnfinishedApplication, Review
 from .queries import (
     create_application,
     get_application_by_user_id,
     user_has_application,
     mark_application_processed,
     get_all_applications,
-    get_unprocessed_applications
+    get_unprocessed_applications,
+    save_unfinished_application,
+    get_unfinished_applications_for_reminder,
+    mark_reminder_sent,
+    get_recent_applications_count,
+    get_recent_applications,
+    get_random_reviews,
+    add_review
 )
 
 __all__ = [
     'init_db',
     'Application',
+    'UnfinishedApplication',
+    'Review',
     'create_application',
     'get_application_by_user_id',
     'user_has_application',
     'mark_application_processed',
     'get_all_applications',
-    'get_unprocessed_applications'
+    'get_unprocessed_applications',
+    'save_unfinished_application',
+    'get_unfinished_applications_for_reminder',
+    'mark_reminder_sent',
+    'get_recent_applications_count',
+    'get_recent_applications',
+    'get_random_reviews',
+    'add_review'
 ]
