5f1e39c57addd04e6d687d9a3da560b048bab5c0
kuboeb <kuboebtilda@gmail.com>
2025-06-14 12:25:16 +0000

    Export info keyboard

---
+++ app/keyboards/__init__.py
@@ -0,0 +1,24 @@
+"""
+Экспорт клавиатур
+"""
+from .user import (
+    get_start_keyboard,
+    get_after_application_keyboard,
+    get_info_keyboard,
+    get_phone_keyboard,
+    get_contact_time_keyboard,
+    get_cancel_keyboard,
+    get_back_to_start_keyboard,
+    get_success_keyboard
+)
+
+__all__ = [
+    'get_start_keyboard',
+    'get_after_application_keyboard',
+    'get_info_keyboard',
+    'get_phone_keyboard',
+    'get_contact_time_keyboard',
+    'get_cancel_keyboard',
+    'get_back_to_start_keyboard',
+    'get_success_keyboard'
+]
