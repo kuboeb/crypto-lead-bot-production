6121789efb9cc10abc29a5894678d69bd16601b0
kuboeb <kuboebtilda@gmail.com>
2025-06-14 14:12:35 +0000

    Update handlers registration with admin panel

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -8,10 +8,15 @@ from .application import router as application_router
 from .info import router as info_router
 from .referral import router as referral_router
 from .commands import router as commands_router
+from app.admin.handlers import register_admin_handlers
 
 
 def register_all_handlers(dp: Dispatcher):
     """Регистрирует все обработчики"""
+    # Сначала регистрируем админку
+    register_admin_handlers(dp)
+    
+    # Затем обычные обработчики
     dp.include_router(commands_router)
     dp.include_router(start_router)
     dp.include_router(application_router)
